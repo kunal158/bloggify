7e97d63ae9a4acefdc79faf27b349da1ee438ed0
KUNAL GOSWAMI <kgreat1508@gmail.com>
2024-06-19 01:03:26 +0530

    cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ connectDB();
 //middlewares
 app.use(express.json());
 // app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
