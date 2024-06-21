ccb3ebbd470b6474087a018cafebfb01bf955a69
KUNAL GOSWAMI <76223437+kunal158@users.noreply.github.com>
2024-06-21 14:48:04 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -3,23 +3,55 @@ const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const Post = require("../models/Post");
-const verifytoken = require("../verifytoken");
+const verifyToken = require("../verifyToken");
+const mongoose = require("mongoose");
+
+// Ensure static routes are defined before dynamic routes
 
 //CREATE
-router.post("/create", verifytoken, async (req, res) => {
+router.post("/create", verifyToken, async (req, res) => {
   try {
     const newPost = new Post(req.body);
-    // console.log(req.body)
     const savedPost = await newPost.save();
-
     res.status(200).json(savedPost);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+//GET USER POSTS
+router.get("/user/:userId", async (req, res) => {
+  try {
+    const posts = await Post.find({ userId: req.params.userId });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET POST DETAILS
+router.get("/:id", async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ error: "Invalid post ID" });
+  }
+  
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //UPDATE
-router.put("/:id", verifytoken, async (req, res) => {
+router.put("/:id", verifyToken, async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ error: "Invalid post ID" });
+  }
+  
   try {
     const updatedPost = await Post.findByIdAndUpdate(
       req.params.id,
@@ -33,7 +65,11 @@ router.put("/:id", verifytoken, async (req, res) => {
 });
 
 //DELETE
-router.delete("/:id", verifytoken, async (req, res) => {
+router.delete("/:id", verifyToken, async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ error: "Invalid post ID" });
+  }
+  
   try {
     await Post.findByIdAndDelete(req.params.id);
     res.status(200).json("Post has been deleted!");
@@ -42,35 +78,14 @@ router.delete("/:id", verifytoken, async (req, res) => {
   }
 });
 
-//GET POST DETAILS
-router.get("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    res.status(200).json(post);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 //GET POSTS
 router.get("/", async (req, res) => {
   const query = req.query;
-
   try {
     const searchFilter = {
       title: { $regex: query.search, $options: "i" },
     };
     const posts = await Post.find(query.search ? searchFilter : null);
-    res.status(200).json(posts);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-//GET USER POSTS
-router.get("/user/:userId", async (req, res) => {
-  try {
-    const posts = await Post.find({ userId: req.params.userId });
     res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
